377b44260faa424533ebf398c0e64af57f4c4a7a
burhanahmeed <burhanahmeed@gmail.com>
2021-10-17 11:24:29 +0700

    done add cypress tag

--- components/DropdownSort.vue
+++ components/DropdownSort.vue
@@ -5,38 +5,38 @@
     </div>
     <div class="bg-white rounded shadow-md absolute top-15" v-show="isDialogOpen">
       <ul>
-        <li class="list border-t" @click="selectSort('terbaru')">
-          <div class="flex items-center space-x-4">
-            <IconsSortAscending class="text-primary" />
-            <span>Terbaru</span>
+        <li class="list border-t" @click="selectSort('terbaru')" data-cy="sort-selection">
+          <div class="flex items-center space-x-4" :data-cy="sortBy === 'terbaru' && 'sort-selection-selected'">
+            <IconsSortAscending data-cy="sort-selection-icon" class="text-primary" />
+            <span data-cy="sort-selection-title">Terbaru</span>
           </div>
           <span v-if="sortBy === 'terbaru'">&#10003;</span>
         </li>
-        <li class="list" @click="selectSort('terlama')">
-          <div class="flex items-center space-x-4">
-            <IconsSortDescending class="text-primary" />
-            <span>Terlama</span>
+        <li class="list" @click="selectSort('terlama')" data-cy="sort-selection">
+          <div class="flex items-center space-x-4" :data-cy="sortBy === 'terlama' && 'sort-selection-selected'">
+            <IconsSortDescending data-cy="sort-selection-icon" class="text-primary" />
+            <span data-cy="sort-selection-title">Terlama</span>
           </div>
           <span v-if="sortBy === 'terlama'">&#10003;</span>
         </li>
-        <li class="list" @click="selectSort('a-z')">
-          <div class="flex items-center space-x-4">
-            <IconsSortAscending class="text-primary" />
-            <span>A - Z</span>
+        <li class="list" @click="selectSort('a-z')" data-cy="sort-selection">
+          <div class="flex items-center space-x-4" :data-cy="sortBy === 'a-z' && 'sort-selection-selected'">
+            <IconsSortAscending data-cy="sort-selection-icon" class="text-primary" />
+            <span data-cy="sort-selection-title">A - Z</span>
           </div>
           <span v-if="sortBy === 'a-z'">&#10003;</span>
         </li>
-        <li class="list" @click="selectSort('z-a')">
-          <div class="flex items-center space-x-4">
-            <IconsSortDescending class="text-primary" />
-            <span>Z - A</span>
+        <li class="list" @click="selectSort('z-a')" data-cy="sort-selection">
+          <div class="flex items-center space-x-4" :data-cy="sortBy === 'z-a' && 'sort-selection-selected'">
+            <IconsSortDescending data-cy="sort-selection-icon" class="text-primary" />
+            <span data-cy="sort-selection-title">Z - A</span>
           </div>
           <span v-if="sortBy === 'z-a'">&#10003;</span>
         </li>
-        <li class="list" @click="selectSort('unfinish')">
-          <div class="flex items-center space-x-4">
-            <IconsSwitchVertical class="text-primary" />
-            <span>Belum Selesai</span>
+        <li class="list" @click="selectSort('unfinish')" data-cy="sort-selection">
+          <div class="flex items-center space-x-4" :data-cy="sortBy === 'unfinish' && 'sort-selection-selected'">
+            <IconsSwitchVertical data-cy="sort-selection-icon" class="text-primary" />
+            <span data-cy="sort-selection-title">Belum Selesai</span>
           </div>
           <span v-if="sortBy === 'unfinish'">&#10003;</span>
         </li>
